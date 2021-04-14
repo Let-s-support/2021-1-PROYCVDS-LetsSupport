2bd1cbddc2da6e2ca34031bc53c78c51fea3b359
JCPosso <JCPosso@users.noreply.github.com>
2021-04-14 12:21:17 -0500

    User Services & User Services Impl OK

---
+++ lets-support/src/main/java/edu/eci/cvds/services/UserServiceException.java
@@ -0,0 +1,7 @@
+package edu.eci.cvds.services;
+
+public class UserServiceException  extends Exception{
+    public UserServiceException (String string) {
+        super(string);
+    }
+}
--- lets-support/src/main/java/edu/eci/cvds/services/UserServices.java
+++ lets-support/src/main/java/edu/eci/cvds/services/UserServices.java
@@ -1,7 +1,44 @@
 package edu.eci.cvds.services;
+import java.util.List;
 
-public class UserServices {
-    public UserServices(){
-        
-    }
+import edu.eci.cvds.entities.Rol;
+import edu.eci.cvds.entities.User;
+
+public interface UserServices {
+    
+    /**
+   * @obj add user to Database
+   * @param id user Identifier
+   * @param fullName first and last User name 
+   * @param username short name
+   * @param passwd unique password identified
+   * @param rol rol as Student, Professor, administrative, graduate, administrator
+   * @param isActive if the user is active in the system
+   * @throws UserServiceException if the information is not complete or if the identifier already exists
+   */
+    public abstract void addUser(int id, String fullName, String username, String passwd, Rol rol, boolean isActive) throws UserServiceException;
+
+    /**
+   * @obj see user' info 
+   * @param id user Identifier
+   * @return Basic User info  identified by 'id' 
+   * @throws UserServiceException If identifier is wrong
+   */   
+    public abstract User seeUser(int id) throws UserServiceException;
+    //public abstract User seeRolUser() throws UserServiceException;
+
+    /**
+   * @obj see user's info 
+   * @return List with Basic Users info 
+   * @throws UserServiceException 
+   */   
+    public abstract List<User> seeUsers() throws UserServiceException;
+
+    
+    /**
+   * @obj see which users are active or inactive in the system
+   * @return List with id,fullname,username and user state
+   * @throws UserServiceException 
+   */   
+    public abstract List<User> seeActiveUsers() throws UserServiceException;
 }
