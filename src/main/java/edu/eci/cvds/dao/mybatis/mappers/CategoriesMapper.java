f716c1f5a3b589483e04a57e964d8d37bff87895
Richard Urrea <rsug.rsug@gmail.com>
2021-04-17 18:26:17 -0500

    instanciado con atributos

--- src/main/java/edu/eci/cvds/dao/mybatis/mappers/CategoriesMapper.java
+++ src/main/java/edu/eci/cvds/dao/mybatis/mappers/CategoriesMapper.java
@@ -7,9 +7,11 @@ import java.util.List;
 public interface CategoriesMapper {
     /**
      * Envia la información que viene de MyBatisCategoriesDAO hacia CategoriesMapper.xml, a través de param para registrar en la base de datos
-     * @param categorie objeto de tipo categoria, que contiene los datos de la nueva categoria que se va a crear
+  //   * @param categorie objeto de tipo categoria, que contiene los datos de la nueva categoria que se va a crear
      */
-     void agregarCategoria(@Param("category") Categories categorie);
+     void agregarCategoria(@Param("value") String value,
+                           @Param("status") String status,
+                           @Param("description") String description);
 
     /**
      * Envia la información que viene de MyBatisCategories y lo envia a CategoriesMapper.xml para realizar la moficacion de los valores de la categoria
